1c32fe58059374304d864612cbbf438e5696ab51
Deepak Mathew <deepak_mathew@hotmail.com>
2019-02-20 08:25:02 +0530

    Update server.js

--- portcart/server.js
+++ portcart/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello PortcartA\n');
+  res.send('Hello PortcartA-Deployment\n');
 });
 
 app.listen(PORT, HOST);
